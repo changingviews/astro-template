de726946a18c12200e5c231422cfb7cf2379d50a
Aaron <aaron.staes@gmail.com>
2023-09-22 20:25:02 -0400

    feat: added Services & usp section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,65 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Barlow', 'sans-serif'],
+		},
 		extend: {
 			colors: {
 				black: '#1F1F1F',
 				white: '#F6F6F6',
-				primary: '#B56576',
-				accent: '#FFB452',
+				primary: '#FFB452',
+				accent: '#028090',
+			},
+			keyframes: {
+				openclose: {
+					'0%': {
+						top: '0',
+					},
+					'5%': {
+						top: '0',
+					},
+
+					'30%': {
+						top: '0',
+					},
+					'33%': {
+						top: '0',
+					},
+					'35%': {
+						top: '0',
+					},
+					'38%': {
+						top: '-3rem',
+					},
+					'48%': {
+						top: '-3rem',
+					},
+					'62%': {
+						top: '-3rem',
+					},
+					'66%': {
+						top: '-3rem',
+					},
+					'71%': {
+						top: '-6rem',
+					},
+					'86%': {
+						top: '-6rem',
+					},
+					'95%': {
+						top: '-6rem',
+					},
+					'98%': {
+						top: '-6rem',
+					},
+					'100%': {
+						top: '0',
+					},
+				},
+			},
+			animation: {
+				openclose: 'openclose 5s ease-in-out infinite',
 			},
 		},
 	},
